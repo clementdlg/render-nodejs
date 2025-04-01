88d0a613c04dbaae978cb2e87e4c50b9eb6d5e7c
Clément DE LA GENIERE <0xkrem@proton.me>
2025-04-01 16:11:16 +0200

    make db.js use env credentials

--- db.js
+++ db.js
@@ -1,24 +1,25 @@
+require('dotenv').config();
 const { Sequelize } = require('sequelize')
 
 // database
 const sequelize = new Sequelize(
-  '', // Database name
-  '', // User
-  '', // Password
-  {
-    host: '', // Host
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-    define: {
-      createdAt: 'added',
-      updatedAt: 'updated',
-    }
-  },
+	process.env.PG_DATABASE, // Database name
+	process.env.PG_USER, // User
+	process.env.PG_PASSWORD, // Password
+	{
+		host: '', // Host
+		dialect: 'postgres',
+		dialectOptions: {
+			ssl: {
+				require: true,
+				rejectUnauthorized: false,
+			},
+		},
+		define: {
+			createdAt: 'added',
+			updatedAt: 'updated',
+		}
+	},
 )
 
 sequelize.authenticate()
